48a798bc8b10600d6e92790ead1d45958320e12f
yoonsung.eth <ys.choi@me.com>
2021-12-05 17:45:54 +0900

    mid

---
+++ contracts/tokens/StandardNFT.sol
@@ -0,0 +1,14 @@
+/**
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+pragma solidity ^0.8.0;
+
+import "@beandao/contracts/library/Initializer.sol";
+import "@beandao/contracts/library/Ownership.sol";
+import "@beandao/contracts/library/Multicall.sol";
+import "../ITemplateV1.sol";
+
+// mint와 burn 필요.
+// mint는 오너 서명에 의해 생성될 수 있는 기능 필요.
+contract StandardNFT {
+}
